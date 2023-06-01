c6ee565db82d49a09342634632b4676fbdd38db8
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-06-01 09:27:05 +0200

    First Commit

---
+++ src/test/java/projectpolaris/ProjectPolarisApplicationTests.java
@@ -0,0 +1,13 @@
+package projectpolaris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPolarisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

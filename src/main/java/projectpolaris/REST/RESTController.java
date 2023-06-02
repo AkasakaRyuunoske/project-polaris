ac721f91456297682da70ca0b7918ce7f3561f9c
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-06-02 21:37:09 +0200

    First Commit

---
+++ src/main/java/projectpolaris/REST/RESTController.java
@@ -0,0 +1,8 @@
+package projectpolaris.REST;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RESTController {
+}

87c2198f89165f001673bcc0dede04b03687c530
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-06-02 21:39:23 +0200

    useless stuff commented

--- src/main/java/projectpolaris/ProjectPolarisApplication.java
+++ src/main/java/projectpolaris/ProjectPolarisApplication.java
@@ -1,19 +1,42 @@
 package projectpolaris;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.kafka.annotation.KafkaListener;
+import projectpolaris.Utilities.StartUpManager;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@Log4j2
 public class ProjectPolarisApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjectPolarisApplication.class, args);
+		ConfigurableApplicationContext context = new SpringApplicationBuilder(ProjectPolarisApplication.class)
+				.listeners(new StartUpManager()).run();
+
+		//#Default constructor, might be useful.
+//		SpringApplication.run(ProjectPolarisApplication.class, args);
 
 		//#Todo Enable Eureka
 //		new SpringApplicationBuilder(ProjectPolarisApplication.class).web(WebApplicationType.NONE).run(args);
 	}
+
+	@KafkaListener(id = "Midir", topics = "UtsuP")
+	public void listen(String in) {
+		log.info(in);
+	}
+
+//	@Bean
+//	public ApplicationRunner runner(KafkaTemplate<String, String> template) {
+//		return args -> {
+//			for (int i = 0; i < 10; i++) {
+//				Thread.sleep(6000); //six sec
+//				template.send("UtsuP", i + ":" +
+//						"\"Ang kahalagahan ng tiyaga at pagtitiyaga sa buhay ay tulad ng paghahanda ng maalamat na ibon na lilipad at tatawid sa malawak na kalangitan. Sa pamamagitan ng puspusang pagsisikap at matatag na paninindigan, malalampasan natin ang mga hamon at mararating natin ang mga pangarap na hindi natin inakala na maabot.\"");
+//			}
+//		};
+//	}
 }

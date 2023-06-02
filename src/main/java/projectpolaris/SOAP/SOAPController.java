ea0310c8ed228fe758df29fda7beb8f38ffccb46
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-06-02 21:36:53 +0200

    First Commit

---
+++ src/main/java/projectpolaris/SOAP/SOAPController.java
@@ -0,0 +1,9 @@
+package projectpolaris.SOAP;
+
+import org.springframework.stereotype.Controller;
+
+
+// #Todo check if spring supports natively SOAP (like RestController or smth)
+@Controller
+public class SOAPController {
+}

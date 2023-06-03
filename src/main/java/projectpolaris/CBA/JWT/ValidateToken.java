74ed87e42057f8c9ebb4794ee80cf481ce4ee393
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-06-03 17:00:01 +0200

    Token validation methods added

--- src/main/java/projectpolaris/CBA/JWT/ValidateToken.java
+++ src/main/java/projectpolaris/CBA/JWT/ValidateToken.java
@@ -1,7 +1,27 @@
 package projectpolaris.CBA.JWT;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+
 @Component
+@Log4j2
 public class ValidateToken {
+    @Autowired
+    JWTUtils jwtUtils;
+
+    // Validation Methods
+    private Boolean isTokenExpired(String token){
+        return jwtUtils.extractExpirationDate(token).before(new Date());
+    }
+
+    public Boolean isTokenValid(String token, UserDetails userDetails){
+        final String username = jwtUtils.extractUserName(token);
+        log.info("Token : " + token);
+
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
 }

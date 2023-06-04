81051dac38c2a8c04bcf57df2f541a37b83efe94
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-06-04 23:08:12 +0200

    Handshake almost done

--- src/main/java/projectpolaris/CBA/Handshake/Camellia.java
+++ src/main/java/projectpolaris/CBA/Handshake/Camellia.java
@@ -72,19 +72,19 @@ public class Camellia {
 //        System.out.println("Decrypted message: " + decryptedMessage);
     }
 
-    public void generateSymmetricKeys(byte[] iv, String key) throws IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException, InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException, NoSuchProviderException {
+    public void generateSymmetricKeys(byte[] iv, byte[] key) throws IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException, InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException, NoSuchProviderException {
 
         Security.addProvider(new BouncyCastleProvider());
 
         // Generate a random IV
-        byte[] ivBytes = new byte[16];
-        SecureRandom secureRandom = new SecureRandom();
-        secureRandom.nextBytes(ivBytes);
-        ivParameterSpec = new IvParameterSpec(ivBytes);
+//        byte[] ivBytes = new byte[16];
+//        SecureRandom secureRandom = new SecureRandom();
+//        secureRandom.nextBytes(ivBytes);
+        ivParameterSpec = new IvParameterSpec(iv);
 
 //        // Create a Camellia cipher instance and initialize it with the key and IV
         cipher = Cipher.getInstance(algorithm_transformation, "BC");
-        secretKeySpec = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), algorithm_name);
+        secretKeySpec = new SecretKeySpec(key, algorithm_name);
 //        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParameterSpec);
 
         // Encrypt the message
@@ -112,14 +112,74 @@ public class Camellia {
 //        System.out.println("Decrypted message: " + decryptedMessage);
     }
 
-    public String enchant(String plain_string){
+    public String enchant(String plain_string) throws NoSuchPaddingException, NoSuchAlgorithmException, NoSuchProviderException, InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
         log.info("Enchant was called!");
-        return null; //#todo write enchant logic
+
+        Security.addProvider(new BouncyCastleProvider());
+
+        // Create a Camellia cipher instance and initialize it with the key and IV
+        Cipher cipher = Cipher.getInstance(algorithm_transformation, "BC");
+
+        log.info("secretKeySpec: " + secretKeySpec);
+        log.info("secretKeySpec: " + secretKeySpec.toString());
+
+        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParameterSpec);
+
+        // Encrypt the message
+        byte[] enchantedBytes = cipher.doFinal(plain_string.getBytes(StandardCharsets.UTF_8));
+
+        // Encode the encrypted bytes and IV using Base64 for convenient display or transmission
+        String enchantedMessage = Base64.getEncoder().encodeToString(enchantedBytes);
+
+        log.info("String before enchant: " + plain_string );
+        log.info("enchantedMessage: " + enchantedMessage);
+
+        return enchantedMessage; //#todo write enchant logic
     }
 
     public String disenchant(String enchanted_string){
         log.info("Disenchant was called!");
 
+//        Security.addProvider(new BouncyCastleProvider());
+//
+//        String message = "Hello, World!";
+//        String key = "ThisIsASecretKey";
+//
+//        // Generate a random IV
+//        byte[] ivBytes = new byte[16];
+//        SecureRandom secureRandom = new SecureRandom();
+//        secureRandom.nextBytes(ivBytes);
+//        IvParameterSpec ivParameterSpec = new IvParameterSpec(ivBytes);
+//
+//        // Create a Camellia cipher instance and initialize it with the key and IV
+//        Cipher cipher = Cipher.getInstance("Camellia/CBC/PKCS7Padding", "BC");
+//        SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), "Camellia");
+//        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParameterSpec);
+//
+//        // Encrypt the message
+//        byte[] encryptedBytes = cipher.doFinal(message.getBytes(StandardCharsets.UTF_8));
+//
+//        // Encode the encrypted bytes and IV using Base64 for convenient display or transmission
+//        String encryptedMessage = Base64.getEncoder().encodeToString(encryptedBytes);
+//        String encodedIV = Base64.getEncoder().encodeToString(ivBytes);
+//        System.out.println("Encrypted message: " + encryptedMessage);
+//        System.out.println("IV: " + encodedIV);
+//
+//        // Create a new cipher instance for decryption and initialize it with the key and IV
+//        Cipher decryptionCipher = Cipher.getInstance("Camellia/CBC/PKCS7Padding", "BC");
+//        decryptionCipher.init(Cipher.DECRYPT_MODE, secretKeySpec, ivParameterSpec);
+//
+//        // Decode the Base64 encoded message and IV
+//        byte[] decodedBytes = Base64.getDecoder().decode(encryptedMessage);
+//        byte[] decodedIV = Base64.getDecoder().decode(encodedIV);
+//
+//        // Decrypt the message
+//        byte[] decryptedBytes = decryptionCipher.doFinal(decodedBytes);
+//
+//        // Convert the decrypted bytes back to a string
+//        String decryptedMessage = new String(decryptedBytes, StandardCharsets.UTF_8);
+//        System.out.println("Decrypted message: " + decryptedMessage);
+//
         return null; //#todo write disenchant logic
     }
 

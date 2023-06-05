12ecfe2568fde035c4ba4a518cbcf707af60fa0e
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-06-05 18:02:56 +0200

    Kafka update: some new topics added

--- src/main/java/projectpolaris/Messaging/KafkaTopics.java
+++ src/main/java/projectpolaris/Messaging/KafkaTopics.java
@@ -2,14 +2,30 @@ package projectpolaris.Messaging;
 
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
-import org.springframework.stereotype.Component;
 
-@Component
+@Configuration
 public class KafkaTopics {
     @Bean
-    public NewTopic topic() {
+    public NewTopic UtsuP() {
         return TopicBuilder.name("UtsuP")
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public NewTopic security() {
+        return TopicBuilder.name("security")
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public NewTopic security1() {
+        return TopicBuilder.name("security")
                 .partitions(1)
                 .replicas(1)
                 .build();

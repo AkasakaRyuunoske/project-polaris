77584de6ed691857632eba0e5474de7c7be85da9
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-06-20 21:35:29 +0200

    GUI POC added

--- src/main/java/projectpolaris/ProjectPolarisApplication.java
+++ src/main/java/projectpolaris/ProjectPolarisApplication.java
@@ -3,9 +3,7 @@ package projectpolaris;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.ConfigurableApplicationContext;
-import projectpolaris.Utilities.StartUpManager;
+import projectpolaris.Beginning_of_a_Dream.MainFrame;
 
 @SpringBootApplication
 @EnableAutoConfiguration
@@ -13,8 +11,10 @@ import projectpolaris.Utilities.StartUpManager;
 public class ProjectPolarisApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = new SpringApplicationBuilder(ProjectPolarisApplication.class)
-				.listeners(new StartUpManager()).run();
+		new MainFrame();
+
+//		ConfigurableApplicationContext context = new SpringApplicationBuilder(ProjectPolarisApplication.class)
+//				.listeners(new StartUpManager()).run();
 
 		//#Default constructor, might be useful.
 //		SpringApplication.run(ProjectPolarisApplication.class, args);

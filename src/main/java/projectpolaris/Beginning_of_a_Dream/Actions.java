b41fbbf1817f250032e7989895504276f00f54cf
Akasaka Ryuunoske <akasakaryuunoske1@gmail.com>
2023-06-29 21:43:08 +0200

    Beginning of the Dream component added

---
+++ src/main/java/projectpolaris/Beginning_of_a_Dream/Actions.java
@@ -0,0 +1,11 @@
+package projectpolaris.Beginning_of_a_Dream;
+
+public enum Actions {
+    Start_The_Application,
+    Shut_Down_The_Application,
+    Change_Configuration,
+    Open_Logs,
+    Open_Certificates,
+    Open_Handshake_Sessions,
+    Open_In_Browser;
+}
